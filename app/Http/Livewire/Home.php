66d36fa1fb99d9913fc05c24b3dffa7bdab92308
Cah Bantul <fardan.nozami@gmail.com>
2022-04-16 08:51:41 +0700

    home

--- app/Http/Livewire/Home.php
+++ app/Http/Livewire/Home.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Produk;
 use Livewire\Component;
 
 class Home extends Component
 {
+    public $produk = [];
     public function render()
     {
+        $this->produk = Produk::all();
         return view('livewire.home')
         ->extends('layouts.app')->section('content');
     }
